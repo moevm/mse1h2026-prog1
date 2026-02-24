0a3f199bd15da839d8e846c128dc21dbca93de75
Kunakova Mariya <kunakovamaria@mail.ru>
2026-02-25 00:34:57 +0300

    Исправление __init__ файла в src #11

--- taskgen/src/__init__.py
+++ taskgen/src/__init__.py
@@ -1,21 +1,25 @@
-from .base_module import BaseTaskClass
+from .base_module.base_task import BaseTaskClass
 import importlib
 import os
 import sys
 import inspect
 
-def load_task_modules():
-    cur_loc = os.path.dirname(__file)
-    package_name = package or "src"
+def __load_task_modules():
+    cur_loc = os.path.dirname(__file__)
+    package_name = __package__ or "src"
 
     for name in os.listdir(cur_loc):
         if name.startswith("module_") and os.path.isdir(os.path.join(cur_loc, name)):
             try:
                 mod = importlib.import_module(f".{name}", package_name)
+                
                 for obj_name, obj in inspect.getmembers(mod):
-                    if inspect.isclass(obj) and issubclass(obj, BaseTaskClass) and obj is not BaseTaskClass:
-                        setattr(sys.modules[name], obj_name, obj)
-            except ImportError:
-                continue
+                    if (inspect.isclass(obj) and 
+                        issubclass(obj, BaseTaskClass) and 
+                        obj is not BaseTaskClass):
+                        
+                        setattr(sys.modules[__name__], obj_name, obj)
+            except Exception as e:
+                print(f"Ошибка загрузки модуля {name}: {e}")
 
 __load_task_modules()
