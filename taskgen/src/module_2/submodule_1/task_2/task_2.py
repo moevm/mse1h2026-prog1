e80353d88ea9987e0eba08dcb9a17634099f5479
typa1yaaa <k.kopasova90@mail.ru>
2026-03-24 12:22:30 +0300

    Исправление task_2 #32

--- taskgen/src/module_2/submodule_1/task_2/task_2.py
+++ taskgen/src/module_2/submodule_1/task_2/task_2.py
@@ -18,7 +18,7 @@ class Module2BuildStagesTask2(BaseTaskClass):
     def generate_task(self):
         action = self._pick_variant()
         self.task_text = (
-            f"На каком этапе сборки выполняется действие: {action}?"
+            f"На каком этапе сборки {action}?"
         )
 
     def init_task(self):
