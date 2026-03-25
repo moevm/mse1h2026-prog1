0850106029a74c8ee352c564d98ff863797b2443
Samoilova Ekaterina <labaproga1@gmail.com>
2026-03-25 23:41:12 +0300

    Обновление __init__.py

--- taskgen/src/module_3/__init__.py
+++ taskgen/src/module_3/__init__.py
@@ -1,7 +1,9 @@
-from .submodule_1.task_1 import Task1CLIParser, Task1
+from .submodule_1.task_1 import Module3_Submodule1_Task1, Module3_Submodule1_Task1CLIParser
 from .submodule_2.task_1 import Module3_Submodule2_Task1, Module3_Submodule2_Task1CLIParser
+from .submodule_2.task_2 import Module3_Submodule2_Task2, Module3_Submodule2_Task2CLIParser
 from .submodule_2.task_5 import Module3_Submodule2_Task5, Module3_Submodule2_Task5CLIParser
 
-__all__ = ["Task1",
+__all__ = ["Module3_Submodule1_Task1",
           "Module3_Submodule2_Task1",
+          "Module3_Submodule2_Task2",
           "Module3_Submodule2_Task5"]
