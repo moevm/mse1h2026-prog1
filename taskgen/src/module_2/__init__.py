604c767f85d819a6d79d36471001acb00cf853c9
typa1yaaa <k.kopasova90@mail.ru>
2026-04-14 23:50:20 +0300

    Исправление __init__ #48

--- taskgen/src/module_2/__init__.py
+++ taskgen/src/module_2/__init__.py
@@ -1,3 +1,15 @@
-from .submodule_1.task_1 import cli_parser, Module2BuildStagesTask1
+from .submodule_1.task_1 import cli_parser as module_2_submodule_1_task_1
+from .submodule_1.task_2 import cli_parser as module_2_submodule_1_task_2
+from .submodule_1.task_3 import cli_parser as module_2_submodule_1_task_3
+from .submodule_1.task_4 import cli_parser as module_2_submodule_1_task_4
+from .submodule_1.task_5 import cli_parser as module_2_submodule_1_task_5
+from .submodule_1.task_6 import cli_parser as module_2_submodule_1_task_6
 
-__all__ = ["cli_parser", "Module2BuildStagesTask1"]
+__all__ = [
+    "module_2_submodule_1_task_1",
+    "module_2_submodule_1_task_2",
+    "module_2_submodule_1_task_3",
+    "module_2_submodule_1_task_4",
+    "module_2_submodule_1_task_5",
+    "module_2_submodule_1_task_6",
+]
