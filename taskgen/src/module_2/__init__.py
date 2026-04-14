e0e664639b8d62359dfdbb691bebcf64d6e56fbc
typa1yaaa <k.kopasova90@mail.ru>
2026-04-15 01:38:59 +0300

    Исправление __init__ #48

--- taskgen/src/module_2/__init__.py
+++ taskgen/src/module_2/__init__.py
@@ -5,6 +5,13 @@ from .submodule_1.task_4 import cli_parser as module_2_submodule_1_task_4
 from .submodule_1.task_5 import cli_parser as module_2_submodule_1_task_5
 from .submodule_1.task_6 import cli_parser as module_2_submodule_1_task_6
 
+from .submodule_2.task_1 import cli_parser as module_2_submodule_2_task_1
+from .submodule_2.task_2 import cli_parser as module_2_submodule_2_task_2
+from .submodule_2.task_3 import cli_parser as module_2_submodule_2_task_3
+from .submodule_2.task_4 import cli_parser as module_2_submodule_2_task_4
+from .submodule_2.task_5 import cli_parser as module_2_submodule_2_task_5
+from .submodule_2.task_5 import cli_parser as module_2_submodule_2_task_6
+
 __all__ = [
     "module_2_submodule_1_task_1",
     "module_2_submodule_1_task_2",
@@ -12,4 +19,11 @@ __all__ = [
     "module_2_submodule_1_task_4",
     "module_2_submodule_1_task_5",
     "module_2_submodule_1_task_6",
+
+    "module_2_submodule_2_task_1",
+    "module_2_submodule_2_task_2",
+    "module_2_submodule_2_task_3",
+    "module_2_submodule_2_task_4",
+    "module_2_submodule_2_task_5",
+    "module_2_submodule_2_task_6",
 ]
