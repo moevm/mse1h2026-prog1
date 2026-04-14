66c967cf4ef1099d74df3a51df966a61cdcd8f07
typa1yaaa <k.kopasova90@mail.ru>
2026-04-14 23:49:27 +0300

    Исправление файлов task_5 #48

---
+++ taskgen/src/module_2/submodule_1/task_5/task_5_cli.py
@@ -0,0 +1,19 @@
+from src.base_module.base_cli import CLIParser
+from .task_5 import Module2BuildStagesTask5
+
+
+def _factory(args):
+    return Module2BuildStagesTask5(seed=args.seed)
+
+
+def _add_args(parser):
+    parser.add_argument("--mode", required=True, choices=["init", "check", "dry-run"])
+    parser.add_argument("--seed", required=True, type=int)
+    parser.add_argument("--solution", default="")
+    parser.set_defaults(func=_factory)
+
+
+cli_parser = CLIParser(
+    name="module_2.submodule_1.task_5",
+    add_cli_args=_add_args,
+)
