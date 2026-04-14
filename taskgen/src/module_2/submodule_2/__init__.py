91070bd427264b37654005f8f7399a70b006c3da
typa1yaaa <k.kopasova90@mail.ru>
2026-04-15 01:39:18 +0300

    Исправление __init__ #48

---
+++ taskgen/src/module_2/submodule_2/__init__.py
@@ -0,0 +1,15 @@
+from .task_1 import Module2PreprocessorTask1
+from .task_2 import Module2PreprocessorTask2
+from .task_3 import Module2PreprocessorTask3
+from .task_4 import Module2PreprocessorTask4
+from .task_5 import Module2PreprocessorTask5
+from .task_6 import Module2PreprocessorTask6
+
+__all__ = [
+    "Module2PreprocessorTask1",
+    "Module2PreprocessorTask2",
+    "Module2PreprocessorTask3",
+    "Module2PreprocessorTask4",
+    "Module2PreprocessorTask5",
+    "Module2PreprocessorTask6",
+]
