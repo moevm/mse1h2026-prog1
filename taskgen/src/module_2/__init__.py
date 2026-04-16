1390d0914e9ecd44522916c01bb524c42345c33b
typa1yaaa <k.kopasova90@mail.ru>
2026-04-16 14:43:42 +0300

    Изменение __init__.py #48

--- taskgen/src/module_2/__init__.py
+++ taskgen/src/module_2/__init__.py
@@ -10,7 +10,13 @@ from .submodule_2.task_2 import cli_parser as module_2_submodule_2_task_2
 from .submodule_2.task_3 import cli_parser as module_2_submodule_2_task_3
 from .submodule_2.task_4 import cli_parser as module_2_submodule_2_task_4
 from .submodule_2.task_5 import cli_parser as module_2_submodule_2_task_5
-from .submodule_2.task_5 import cli_parser as module_2_submodule_2_task_6
+from .submodule_2.task_6 import cli_parser as module_2_submodule_2_task_6
+from .submodule_2.task_7 import cli_parser as module_2_submodule_2_task_7
+from .submodule_2.task_8 import cli_parser as module_2_submodule_2_task_8
+from .submodule_2.task_9 import cli_parser as module_2_submodule_2_task_9
+from .submodule_2.task_10 import cli_parser as module_2_submodule_2_task_10
+from .submodule_2.task_11 import cli_parser as module_2_submodule_2_task_11
+from .submodule_2.task_12 import cli_parser as module_2_submodule_2_task_12
 
 __all__ = [
     "module_2_submodule_1_task_1",
@@ -26,4 +32,10 @@ __all__ = [
     "module_2_submodule_2_task_4",
     "module_2_submodule_2_task_5",
     "module_2_submodule_2_task_6",
+    "module_2_submodule_2_task_7",
+    "module_2_submodule_2_task_8",
+    "module_2_submodule_2_task_9",
+    "module_2_submodule_2_task_10",
+    "module_2_submodule_2_task_11",
+    "module_2_submodule_2_task_12",
 ]
