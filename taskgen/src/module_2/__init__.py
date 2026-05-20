89be01a3a3b7e29eb84077e01ec19f1f83126995
Kopasova Ksenia <139976614+typa1yaaa@users.noreply.github.com>
2026-05-20 13:49:25 +0300

    Устранение конфликта модулей

--- taskgen/src/module_2/__init__.py
+++ taskgen/src/module_2/__init__.py
@@ -1,98 +1,98 @@
-from .submodule_1.task_1 import cli_parser as module_2_submodule_1_task_1
-from .submodule_1.task_2 import cli_parser as module_2_submodule_1_task_2
-from .submodule_1.task_3 import cli_parser as module_2_submodule_1_task_3
-from .submodule_1.task_4 import cli_parser as module_2_submodule_1_task_4
-from .submodule_1.task_5 import cli_parser as module_2_submodule_1_task_5
-from .submodule_1.task_6 import cli_parser as module_2_submodule_1_task_6
+from .submodule_1.task_1 import Module2_Submodule1_Task1_CLIParser, Module2_Submodule1_Task1
+from .submodule_1.task_2 import Module2_Submodule1_Task2_CLIParser, Module2_Submodule1_Task2
+from .submodule_1.task_3 import Module2_Submodule1_Task3_CLIParser, Module2_Submodule1_Task3
+from .submodule_1.task_4 import Module2_Submodule1_Task4_CLIParser, Module2_Submodule1_Task4
+from .submodule_1.task_5 import Module2_Submodule1_Task5_CLIParser, Module2_Submodule1_Task5
+from .submodule_1.task_6 import Module2_Submodule1_Task6_CLIParser, Module2_Submodule1_Task6
 
-from .submodule_2.task_1 import cli_parser as module_2_submodule_2_task_1
-from .submodule_2.task_2 import cli_parser as module_2_submodule_2_task_2
-from .submodule_2.task_3 import cli_parser as module_2_submodule_2_task_3
-from .submodule_2.task_4 import cli_parser as module_2_submodule_2_task_4
-from .submodule_2.task_5 import cli_parser as module_2_submodule_2_task_5
-from .submodule_2.task_6 import cli_parser as module_2_submodule_2_task_6
-from .submodule_2.task_7 import cli_parser as module_2_submodule_2_task_7
-from .submodule_2.task_8 import cli_parser as module_2_submodule_2_task_8
-from .submodule_2.task_9 import cli_parser as module_2_submodule_2_task_9
-from .submodule_2.task_10 import cli_parser as module_2_submodule_2_task_10
-from .submodule_2.task_11 import cli_parser as module_2_submodule_2_task_11
-from .submodule_2.task_12 import cli_parser as module_2_submodule_2_task_12
+from .submodule_2.task_1 import Module2_Submodule2_Task1_CLIParser, Module2_Submodule2_Task1
+from .submodule_2.task_2 import Module2_Submodule2_Task2_CLIParser, Module2_Submodule2_Task2
+from .submodule_2.task_3 import Module2_Submodule2_Task3_CLIParser, Module2_Submodule2_Task3
+from .submodule_2.task_4 import Module2_Submodule2_Task4_CLIParser, Module2_Submodule2_Task4
+from .submodule_2.task_5 import Module2_Submodule2_Task5_CLIParser, Module2_Submodule2_Task5
+from .submodule_2.task_6 import Module2_Submodule2_Task6_CLIParser, Module2_Submodule2_Task6
+from .submodule_2.task_7 import Module2_Submodule2_Task7_CLIParser, Module2_Submodule2_Task7
+from .submodule_2.task_8 import Module2_Submodule2_Task8_CLIParser, Module2_Submodule2_Task8
+from .submodule_2.task_9 import Module2_Submodule2_Task9_CLIParser, Module2_Submodule2_Task9
+from .submodule_2.task_10 import Module2_Submodule2_Task10_CLIParser, Module2_Submodule2_Task10
+from .submodule_2.task_11 import Module2_Submodule2_Task11_CLIParser, Module2_Submodule2_Task11
+from .submodule_2.task_12 import Module2_Submodule2_Task12_CLIParser, Module2_Submodule2_Task12
 
-from .submodule_3.task_1 import cli_parser as module_2_submodule_3_task_1
-from .submodule_3.task_2 import cli_parser as module_2_submodule_3_task_2
-from .submodule_3.task_3 import cli_parser as module_2_submodule_3_task_3
-from .submodule_3.task_4 import cli_parser as module_2_submodule_3_task_4
-from .submodule_3.task_5 import cli_parser as module_2_submodule_3_task_5
-from .submodule_3.task_6 import cli_parser as module_2_submodule_3_task_6
+from .submodule_3.task_1 import Module2_Submodule3_Task1_CLIParser, Module2_Submodule3_Task1
+from .submodule_3.task_2 import Module2_Submodule3_Task2_CLIParser, Module2_Submodule3_Task2
+from .submodule_3.task_3 import Module2_Submodule3_Task3_CLIParser, Module2_Submodule3_Task3
+from .submodule_3.task_4 import Module2_Submodule3_Task4_CLIParser, Module2_Submodule3_Task4
+from .submodule_3.task_5 import Module2_Submodule3_Task5_CLIParser, Module2_Submodule3_Task5
+from .submodule_3.task_6 import Module2_Submodule3_Task6_CLIParser, Module2_Submodule3_Task6
 
-from .submodule_4.task_1 import cli_parser as module_2_submodule_4_task_1
-from .submodule_4.task_2 import cli_parser as module_2_submodule_4_task_2
-from .submodule_4.task_3 import cli_parser as module_2_submodule_4_task_3
-from .submodule_4.task_4 import cli_parser as module_2_submodule_4_task_4
-from .submodule_4.task_5 import cli_parser as module_2_submodule_4_task_5
-from .submodule_4.task_6 import cli_parser as module_2_submodule_4_task_6
+from .submodule_4.task_1 import Module2_Submodule4_Task1_CLIParser, Module2_Submodule4_Task1
+from .submodule_4.task_2 import Module2_Submodule4_Task2_CLIParser, Module2_Submodule4_Task2
+from .submodule_4.task_3 import Module2_Submodule4_Task3_CLIParser, Module2_Submodule4_Task3
+from .submodule_4.task_4 import Module2_Submodule4_Task4_CLIParser, Module2_Submodule4_Task4
+from .submodule_4.task_5 import Module2_Submodule4_Task5_CLIParser, Module2_Submodule4_Task5
+from .submodule_4.task_6 import Module2_Submodule4_Task6_CLIParser, Module2_Submodule4_Task6
 
-from .submodule_5.task_1 import cli_parser as module_2_submodule_5_task_1
-from .submodule_5.task_2 import cli_parser as module_2_submodule_5_task_2
-from .submodule_5.task_3 import cli_parser as module_2_submodule_5_task_3
-from .submodule_5.task_4 import cli_parser as module_2_submodule_5_task_4
-from .submodule_5.task_5 import cli_parser as module_2_submodule_5_task_5
+from .submodule_5.task_1 import Module2_Submodule5_Task1_CLIParser, Module2_Submodule5_Task1
+from .submodule_5.task_2 import Module2_Submodule5_Task2_CLIParser, Module2_Submodule5_Task2
+from .submodule_5.task_3 import Module2_Submodule5_Task3_CLIParser, Module2_Submodule5_Task3
+from .submodule_5.task_4 import Module2_Submodule5_Task4_CLIParser, Module2_Submodule5_Task4
+from .submodule_5.task_5 import Module2_Submodule5_Task5_CLIParser, Module2_Submodule5_Task5
 
-from .submodule_6.task_1 import cli_parser as module_2_submodule_6_task_1
-from .submodule_6.task_2 import cli_parser as module_2_submodule_6_task_2
-from .submodule_6.task_3 import cli_parser as module_2_submodule_6_task_3
-from .submodule_6.task_4 import cli_parser as module_2_submodule_6_task_4
-from .submodule_6.task_5 import cli_parser as module_2_submodule_6_task_5
-from .submodule_6.task_6 import cli_parser as module_2_submodule_6_task_6
-from .submodule_6.task_7 import cli_parser as module_2_submodule_6_task_7
+from .submodule_6.task_1 import Module2_Submodule6_Task1_CLIParser, Module2_Submodule6_Task1
+from .submodule_6.task_2 import Module2_Submodule6_Task2_CLIParser, Module2_Submodule6_Task2
+from .submodule_6.task_3 import Module2_Submodule6_Task3_CLIParser, Module2_Submodule6_Task3
+from .submodule_6.task_4 import Module2_Submodule6_Task4_CLIParser, Module2_Submodule6_Task4
+from .submodule_6.task_5 import Module2_Submodule6_Task5_CLIParser, Module2_Submodule6_Task5
+from .submodule_6.task_6 import Module2_Submodule6_Task6_CLIParser, Module2_Submodule6_Task6
+from .submodule_6.task_7 import Module2_Submodule6_Task7_CLIParser, Module2_Submodule6_Task7
 
 
 __all__ = [
-    "module_2_submodule_1_task_1",
-    "module_2_submodule_1_task_2",
-    "module_2_submodule_1_task_3",
-    "module_2_submodule_1_task_4",
-    "module_2_submodule_1_task_5",
-    "module_2_submodule_1_task_6",
+    "Module2_Submodule1_Task1", 
+    "Module2_Submodule1_Task2", 
+    "Module2_Submodule1_Task3", 
+    "Module2_Submodule1_Task4", 
+    "Module2_Submodule1_Task5",
+    "Module2_Submodule1_Task6",  
 
-    "module_2_submodule_2_task_1",
-    "module_2_submodule_2_task_2",
-    "module_2_submodule_2_task_3",
-    "module_2_submodule_2_task_4",
-    "module_2_submodule_2_task_5",
-    "module_2_submodule_2_task_6",
-    "module_2_submodule_2_task_7",
-    "module_2_submodule_2_task_8",
-    "module_2_submodule_2_task_9",
-    "module_2_submodule_2_task_10",
-    "module_2_submodule_2_task_11",
-    "module_2_submodule_2_task_12",
+    "Module2_Submodule2_Task1",
+    "Module2_Submodule2_Task2",
+    "Module2_Submodule2_Task3",
+    "Module2_Submodule2_Task4",
+    "Module2_Submodule2_Task5",
+    "Module2_Submodule2_Task6",
+    "Module2_Submodule2_Task7",
+    "Module2_Submodule2_Task8",
+    "Module2_Submodule2_Task9",
+    "Module2_Submodule2_Task10",
+    "Module2_Submodule2_Task11",
+    "Module2_Submodule2_Task12",
 
-    "module_2_submodule_3_task_1",
-    "module_2_submodule_3_task_2",
-    "module_2_submodule_3_task_3",
-    "module_2_submodule_3_task_4",
-    "module_2_submodule_3_task_5",
-    "module_2_submodule_3_task_6",
+    "Module2_Submodule3_Task1", 
+    "Module2_Submodule3_Task2", 
+    "Module2_Submodule3_Task3", 
+    "Module2_Submodule3_Task4", 
+    "Module2_Submodule3_Task5",
+    "Module2_Submodule3_Task6", 
 
-    "module_2_submodule_4_task_1",
-    "module_2_submodule_4_task_2",
-    "module_2_submodule_4_task_3",
-    "module_2_submodule_4_task_4",
-    "module_2_submodule_4_task_5",
-    "module_2_submodule_4_task_6",
+    "Module2_Submodule4_Task1", 
+    "Module2_Submodule4_Task2", 
+    "Module2_Submodule4_Task3", 
+    "Module2_Submodule4_Task4", 
+    "Module2_Submodule4_Task5",
+    "Module2_Submodule4_Task6",
 
-    "module_2_submodule_5_task_1",
-    "module_2_submodule_5_task_2",
-    "module_2_submodule_5_task_3",
-    "module_2_submodule_5_task_4",
-    "module_2_submodule_5_task_5",
+    "Module2_Submodule5_Task1", 
+    "Module2_Submodule5_Task2", 
+    "Module2_Submodule5_Task3", 
+    "Module2_Submodule5_Task4", 
+    "Module2_Submodule5_Task5",
 
-    "module_2_submodule_6_task_1",
-    "module_2_submodule_6_task_2",
-    "module_2_submodule_6_task_3",
-    "module_2_submodule_6_task_4",
-    "module_2_submodule_6_task_5",
-    "module_2_submodule_6_task_6",
-    "module_2_submodule_6_task_7",
+    "Module2_Submodule6_Task1",
+    "Module2_Submodule6_Task2",
+    "Module2_Submodule6_Task3",
+    "Module2_Submodule6_Task4",
+    "Module2_Submodule6_Task5",
+    "Module2_Submodule6_Task6",
+    "Module2_Submodule6_Task7",
 ]
--- taskgen/src/module_2/submodule_6/task_1/__init__.py
+++ taskgen/src/module_2/submodule_6/task_1/__init__.py
@@ -1,5 +1,5 @@
-from .task_1_cli import cli_parser
-from .task_1 import Module2ComlpilerFlagsTask1
+from .task_1_cli import Module2_Submodule6_Task1_CLIParser
+from .task_1 import Module2_Submodule6_Task1
 
 
-__all__ = ["cli_parser", "Module2ComlpilerFlagsTask1"]
+__all__ = ["Module2_Submodule6_Task1_CLIParser", "Module2_Submodule6_Task1"]
